aea917a752bf9cc5f9e3f34e1d9ff15c263da4df
Levy-Jr <levybusinessjr@gmail.com>
2024-09-20 14:50:14 -0400

    fix: blur overflow in safari and add pablo marques and gabriel leao animation

--- app/(home)/components/marques-and-leao-img.tsx
+++ app/(home)/components/marques-and-leao-img.tsx
@@ -3,7 +3,19 @@
 import Image from "next/image"
 import PabloEGabriel from "/public/marqueseleao/foto-pablo-e-gabriel.webp"
 import { useState } from "react"
+import { motion } from "framer-motion"
 import { cn } from "@/lib/utils"
+
+const sideVariants = {
+  closed: {
+    display: "none",
+    opacity: 0
+  },
+  open: {
+    display: "block",
+    opacity: 1
+  }
+}
 
 const MarquesAndLeaoImg = () => {
   const [showPablo, setShowPablo] = useState(false)
@@ -13,15 +25,20 @@ const MarquesAndLeaoImg = () => {
     <>
       <button
         onClick={() => setShowPablo(!showPablo)}
-        className="absolute top-[2%] left-[3%] bg-black/45 z-10 px-5 py-2 rounded-[100vmax]"
+        className={cn("absolute font-bold top-[2%] left-[8%] bg-black/45 z-10 border border-transparent px-5 py-2 rounded-[100vmax] lg:rounded-xl", showPablo && "bg-transparent border-black text-black")}
       >Conhecer Pablo</button>
-      <div className={cn("hidden bg-white/80 text-black absolute left-0 top-0 w-full h-full", showPablo && "block [clip-path:polygon(100%_0,100%_0,14.5%_100%,0_100%,0_0)]")}>
-        <div className="float-right w-full aspect-square [shape-outside:polygon(100%_0,17%_100%,100%_100%)]"></div>
-        <div>
-          <p className="ml-10 mt-16 font-bold text-3xl font-baskervville">Pablo Marques</p>
-          <p className="ml-10 mt-2 text-[.75rem] md:text-base lg:text-xl">Atuando a mais de 12 anos no mercado imobiliário, tive o privilégio de impactar dezenas de pessoas ao longo da minha trajetório, o que me trouxe expertise quando o assunto é imóveis e transformação de vidas. Com foco em imóveis de médio e alto padrão na região de Novo Hamburgo e Campo Bom, sou especialista em ativos imobiliários e estratégias de marketing imobiliário.</p>
+      <motion.div
+        className="bg-white/80 text-black absolute left-0 top-0 w-full h-full [clip-path:polygon(100%_0,100%_0,15%_100%,0_100%,0_0)]"
+        initial={false}
+        animate={showPablo ? "open" : "closed"}
+        variants={sideVariants}
+      >
+        <div className="float-right w-full h-full [shape-outside:polygon(100%_0,17%_100%,100%_100%)]"></div>
+        <div className="ml-3 sm:ml-10 lg:ml-[8%] mt-[15%] sm:mt-[20%] lg:mt-[12.5%] font-medium max-w-[32.5ch]">
+          <p className="font-bold text-lg sm:text-3xl lg:text-4xl xl:text-5xl font-baskervville">Pablo Marques</p>
+          <p className="mt-2 text-[.75rem] sm:text-base xl:text-lg !leading-[1.3]">Atuando a mais de 12 anos no mercado imobiliário, tive o privilégio de impactar dezenas de pessoas ao longo da minha trajetório, o que me trouxe expertise quando o assunto é imóveis e transformação de vidas. Com foco em imóveis de médio e alto padrão na região de Novo Hamburgo e Campo Bom, sou especialista em ativos imobiliários e estratégias de marketing imobiliário.</p>
         </div>
-      </div>
+      </motion.div>
       <Image
         className="rounded-[.625rem]"
         src={PabloEGabriel}
@@ -29,15 +46,20 @@ const MarquesAndLeaoImg = () => {
       />
       <button
         onClick={() => setShowGabriel(!showGabriel)}
-        className="absolute bottom-[2%] right-[3%] bg-black/45 z-10 px-5 py-2 rounded-[100vmax]"
+        className={cn("absolute bottom-[2%] font-bold right-[3%] bg-black/45 z-10 px-5 border border-transparent transition-colors py-2 rounded-[100vmax] lg:rounded-xl", showGabriel && "bg-transparent border-white")}
       >Conhecer Gabriel</button>
-      <div className={cn("hidden absolute bg-mainPurple/80 right-0 bottom-0 w-full h-full", showGabriel && "block [clip-path:polygon(100%_0,14.6%_100%,100%_100%)]")}>
-        <div className="float-left w-full aspect-square [shape-outside:polygon(100%_0,100%_0,14.5%_100%,0_100%,0_0)]"></div>
-        <div className="text-end ml-auto">
-          <p className="font-semibold text-3xl font-baskervville">Gabriel Leão</p>
-          <p className="text-[.75rem] md:text-base">Meu nome é Gabriel Leão, sou um dos fundados de Marques & Leão, tenho 32 anos, morador de Novo Hamburgo, casado com a Nina, que é dona do meu coração e pai de pet do meu cachorrinho chamado Véio, atuo no mercado imobiliário há quase 11 anos, profissão onde me encontrei e sou apaixonado pelo que faço!</p>
+      <motion.div
+        className="absolute bg-mainPurple/80 right-0 bottom-0 w-full h-full [clip-path:polygon(100%_0,14.6%_100%,100%_100%)]"
+        initial={false}
+        animate={showGabriel ? "open" : "closed"}
+        variants={sideVariants}
+      >
+        <div className="float-left w-full h-full [shape-outside:polygon(100%_0,100%_0,14.5%_100%,0_100%,0_0)]"></div>
+        <div className="text-end font-medium mt-[30%] sm:mt-[68%] lg:mt-[50%] xl:mt-[68%] translate-x-[-.5rem]">
+          <p className="font-semibold text-lg sm:text-3xl lg:text-[2rem] xl:text-[2.75rem] font-baskervville">Gabriel Leão</p>
+          <p className="mt-2 lg:mt-4 text-[.75rem] sm:text-base xl:text-lg !leading-[1.3]">Meu nome é Gabriel Leão, sou um dos fundadores da Marques & Leão, tenho 32 anos, morador de Novo Hamburgo, casado com a Nina, que é dona do meu coração e pai de pet do meu cachorrinho chamado Véio, atuo no mercado imobiliário há quase 11 anos, profissão onde me encontrei e sou apaixonado pelo que faço!</p>
         </div>
-      </div>
+      </motion.div>
     </>
   )
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -114,155 +114,163 @@ export default function Home() {
             </div>
           </article>
         </section>
-        <section className="w-[min(90%,75rem)] relative mt-24 mb-12 mx-auto flex flex-col items-center md:flex-row md:items-center md:justify-between md:gap-20">
-          <Image
-            draggable={false}
-            className="hidden lg:block absolute bottom-[50%] translate-y-[50%] opacity-70 right-[-75%] -z-10 w-[85%]"
-            src={Ellipse}
-            alt="Ellipse blur"
-          />
-          <div className="relative w-[min(100%,30rem)] ml-4 sm:ml-0">
+        <section className="relative">
+          <div className="hidden lg:block absolute overflow-hidden right-0 bottom-1/2 translate-y-[65%] -z-10 w-[50%] aspect-square">
             <Image
-              className="rounded-xl mx-auto md:mx-0"
-              src={Media}
-              alt="Gabriel Leão de costa para foto, de frente para uma casa"
+              draggable={false}
+              className="absolute opacity-70 w-full right-[-40%] h-full"
+              src={Ellipse}
+              alt="Ellipse blur"
             />
-            <div className="text-sm lg:text-base absolute translate-x-[-15%] md:right-0 md:translate-x-[35%] bottom-[35.5%] sm:bottom-[40%] md:bottom-[60%] px-3 py-3 md:px-2 md:py-2 lg:px-4 lg:py-4 translate-y-[50%] backdrop-blur-2xl rounded-[.625rem]">
-              <span className="text-xl lg:text-[clamp(1rem,3vw,1.875rem)] font-bold block text-center">+22mil</span> nas redes sociais
-            </div>
-            <div className="text-sm lg:text-base absolute translate-x-[-15%] md:right-0 md:translate-x-[50%] bottom-[57%] sm:bottom-[60%] md:bottom-[37.5%] px-3 py-3 md:px-2 md:py-2 lg:px-3 lg:py-3 translate-y-1/2 backdrop-blur-2xl rounded-[.625rem]">
-              <span className="text-xl lg:text-[clamp(1rem,3vw,1.875rem)] font-bold block text-center">+1000</span> imóveis vendidos
-            </div>
-            <div className="text-sm lg:text-base absolute translate-x-[-15%] md:right-0 md:translate-x-[40%] bottom-[78.5%] md:bottom-[15%] px-3 py-3 md:px-2 md:py-2 lg:px-4 lg:py-4 translate-y-1/2 backdrop-blur-2xl rounded-[.625rem]">
-              <span className="text-xl lg:text-[clamp(1rem,3vw,1.875rem)] font-bold block">+100 mil</span> de alcance mensal
-            </div>
           </div>
-          <div className="mt-16 md:mt-0">
-            <span className="flex items-center gap-2 text-[#898989] after:inline-block after:w-28 after:h-[1.75px] after:bg-[#898989]">Por que</span>
-            <h2 className={`text-3xl tracking-wide font-baskervville`}>Por que a Marques&Leão?</h2>
-            <p className="mt-3 mb-6 max-w-[40ch] text-[#a7a7a7] leading-5">Somos <span className="text-white">a maior vitrine imobiliária</span> da região, investindo em vídeos, anúncios e inovação.</p>
-            <ul className="grid md:grid-cols-3 *:min-w-fit md:*:min-w-0 md:*:w-[min(100%,12.5rem)] md:*:h-full md:*:aspect-square *:inline-flex md:*:flex-col *:items-center md:*:items-start md:*:justify-between *:gap-2 sm:gap-4 md*:gap-0 *:font-bold text-[.813rem] md:text-sm lg:*:text-lg *:rounded-[.625rem] *:px-2 sm:px-4 md:*:px-2 lg:*:px-4 *:py-2 md:*:py-6 lg:*:py-8 gap-2 sm:gap-x-3 gap-y-3 marques_leao">
-              <li className="bg-mainPurple md:*:w-[min(100%,12.5rem)] md:*:h-full md:*:aspect-square *:inline-flex md:*:flex-col *:items-center md:*:items-start md:*:justify-between *:gap-4 md*:gap-0 *:font-bold lg:*:text-lg *:rounded-[.625rem] !px-4 *:px- *:py-2 md:*:py-6 lg:*:py-8 md:*:px-2 lg:*:px-4">
-                <Link
-                  className="!py-0 md:!py-0 lg:!py-0 !px-0 !text-[.9375rem] md:!text-sm lg:!text-lg"
-                  href="/anunciar-imovel"
-                >
+          <div className="w-[min(90%,75rem)] mt-24 mb-12 mx-auto flex flex-col items-center md:flex-row md:items-center md:justify-between md:gap-20">
+            <div className="relative w-[min(100%,30rem)] ml-4 sm:ml-0">
+              <Image
+                className="rounded-xl mx-auto md:mx-0"
+                src={Media}
+                alt="Gabriel Leão de costa para foto, de frente para uma casa"
+              />
+              <div className="text-sm lg:text-base absolute translate-x-[-15%] md:right-0 md:translate-x-[35%] bottom-[35.5%] sm:bottom-[40%] md:bottom-[60%] px-3 py-3 md:px-2 md:py-2 lg:px-4 lg:py-4 translate-y-[50%] backdrop-blur-2xl rounded-[.625rem]">
+                <span className="text-xl lg:text-[clamp(1rem,3vw,1.875rem)] font-bold block text-center">+22mil</span> nas redes sociais
+              </div>
+              <div className="text-sm lg:text-base absolute translate-x-[-15%] md:right-0 md:translate-x-[50%] bottom-[57%] sm:bottom-[60%] md:bottom-[37.5%] px-3 py-3 md:px-2 md:py-2 lg:px-3 lg:py-3 translate-y-1/2 backdrop-blur-2xl rounded-[.625rem]">
+                <span className="text-xl lg:text-[clamp(1rem,3vw,1.875rem)] font-bold block text-center">+1000</span> imóveis vendidos
+              </div>
+              <div className="text-sm lg:text-base absolute translate-x-[-15%] md:right-0 md:translate-x-[40%] bottom-[78.5%] md:bottom-[15%] px-3 py-3 md:px-2 md:py-2 lg:px-4 lg:py-4 translate-y-1/2 backdrop-blur-2xl rounded-[.625rem]">
+                <span className="text-xl lg:text-[clamp(1rem,3vw,1.875rem)] font-bold block">+100 mil</span> de alcance mensal
+              </div>
+            </div>
+            <div className="mt-16 md:mt-0">
+              <span className="flex items-center gap-2 text-[#898989] after:inline-block after:w-28 after:h-[1.75px] after:bg-[#898989]">Por que</span>
+              <h2 className={`text-3xl tracking-wide font-baskervville`}>Por que a Marques&Leão?</h2>
+              <p className="mt-3 mb-6 max-w-[40ch] text-[#a7a7a7] leading-5">Somos <span className="text-white">a maior vitrine imobiliária</span> da região, investindo em vídeos, anúncios e inovação.</p>
+              <ul className="grid md:grid-cols-3 *:min-w-fit md:*:min-w-0 md:*:w-[min(100%,12.5rem)] md:*:h-full md:*:aspect-square *:inline-flex md:*:flex-col *:items-center md:*:items-start md:*:justify-between *:gap-2 sm:gap-4 md*:gap-0 *:font-bold text-[.813rem] md:text-[.75rem] lg:*:text-lg *:rounded-[.625rem] *:px-2 sm:px-4 md:*:px-1 lg:*:px-4 *:py-2 md:*:py-2 lg:*:py-8 gap-2 sm:gap-x-3 gap-y-3 marques_leao">
+                <li className="bg-mainPurple md:*:w-[min(100%,12.5rem)] md:*:h-full md:*:aspect-square *:inline-flex md:*:flex-col *:items-center md:*:items-start md:*:justify-between *:gap-4 md*:gap-0 *:font-bold lg:*:text-lg *:rounded-[.625rem] !px-4 *:px- *:py-2 md:*:py-6 lg:*:py-8 md:*:px-2 lg:*:px-4">
+                  <Link
+                    className="!py-0 md:!py-0 lg:!py-0 !px-0 !text-[.9375rem] md:!text-[.75rem] lg:!text-lg"
+                    href="/anunciar-imovel"
+                  >
+                    <div className="w-5 lg:w-auto">
+                      <Image
+                        src={WhiteSearchIcon}
+                        alt="Ícone de pesquisa"
+                      />
+                    </div>
+                    <p className="lg:leading-[1.5rem]">Anuncie seu imóvel conosco</p>
+                  </Link>
+                </li>
+                <li className="bg-white text-mainPurple">
                   <div className="w-5 lg:w-auto">
                     <Image
-                      src={WhiteSearchIcon}
-                      alt="Ícone de pesquisa"
+                      src={WhatsappPurpleIcon}
+                      alt="Whatsapp"
                     />
                   </div>
-                  <p className="lg:leading-[1.5rem]">Anuncie seu imóvel conosco</p>
-                </Link>
-              </li>
-              <li className="bg-white text-mainPurple">
-                <div className="w-5 lg:w-auto">
-                  <Image
-                    src={WhatsappPurpleIcon}
-                    alt="Whatsapp"
-                  />
-                </div>
-                <p className="max-w-[15ch] leading-[1rem] lg:leading-[1.5rem] md:max-w-auto">Fale com nossos corretores</p>
-              </li>
-              <li className="bg-white text-mainPurple">
-                <div className="w-5 lg:w-auto">
-                  <Image
-                    src={HandshakeIcon}
-                    alt="Ícones de aperto de mãos"
-                  />
-                </div>
-                <p className="max-w-[15ch] leading-[1rem] lg:leading-[1.5rem] md:max-w-auto">Seja parceiro Marques&Leão</p>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className="w-[min(90%,80rem)] relative mt-36 mx-auto flex flex-col lg:flex-row items-center gap-[clamp(1rem,2.5vw,2.5rem)]">
-          <Image
-            draggable={false}
-            className="hidden lg:block absolute bottom-[-40%] opacity-75 right-[-40%] -z-10 w-[45%]"
-            src={Ellipse}
-            alt="Ellipse blur"
-          />
-          <div className="w-[min(100%,37.5rem)] relative lg:flex-1">
-            <MarquesAndLeaoImg />
-          </div>
-          <div className="w-[min(100%,37.5rem)] sm:mt-4 lg:mt-0 lg:flex-1 flex flex-row-reverse lg:flex-row gap-[clamp(1rem,2vw,2.5rem)]">
-            <div className="w-full lg:max-w-[32ch]">
-              <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-[clamp(2rem,3vw,3rem)] font-bold lg:!leading-[clamp(2rem,4vw,3.5rem)] font-baskervville">Pablo Marques & Gabriel Leão</h2>
-              <p className="sm:text-lg lg:text-lg text-[#d2d2d2] !leading-6 mt-2">Nosso trabalho tem como foco a <span className="text-white font-semibold">conexão entre tecnologia e atendimento de alta performance</span>, com o objetivo de otimizar a jornada e melhorar experiência do cliente.</p>
-              <ul className="hidden lg:block *:flex *:items-center *:gap-5 *:mt-8">
-                <li>
-                  <div>
-                    <Image
-                      className="w-[clamp(4.5rem,7vw,5.625rem)]"
-                      src={MarquesInstagram}
-                      alt="Foto de Pablo Marques e Gabriel Leão"
-                    />
-                  </div>
-                  <div>
-                    <p className="text-[clamp(.875rem,1.5vw,1.125rem)] mb-3">@marqueseleao</p>
-                    <Link
-                      className="flex items-center font-semibold text-sm gap-2 bg-mainPurple rounded-[.635rem] py-3 px-5"
-                      href="#"
-                    >
-                      <Image
-                        src={InstagramIcon}
-                        alt="Instagram"
-                      />
-                      Seguir no Instagram
-                    </Link>
-                  </div>
+                  <p className="max-w-[15ch] leading-[1rem] lg:leading-[1.5rem] md:max-w-auto">Fale com nossos corretores</p>
                 </li>
-                <li>
-                  <div>
+                <li className="bg-white text-mainPurple">
+                  <div className="w-5 lg:w-auto">
                     <Image
-                      className="w-[clamp(4.5rem,7vw,5.625rem)]"
-                      src={MarquesYoutube}
-                      alt="Foto de Pablo Marques e Gabriel Leão"
+                      src={HandshakeIcon}
+                      alt="Ícones de aperto de mãos"
                     />
                   </div>
-                  <div>
-                    <p className="text-[clamp(.875rem,1.5vw,1.125rem)] mb-3">@ImobiliariaMarquesLeao</p>
-                    <Link
-                      className="flex items-center font-semibold text-sm gap-2 max-w-[23ch] bg-mainPurple rounded-[.635rem] py-3 px-5"
-                      href=""
-                    >
-                      <Image
-                        className="w-[1.0625rem]"
-                        src={YoutubeIcon}
-                        alt="Youtube"
-                      />
-                      Se inscreva no canal
-                    </Link>
-                  </div>
-                </li>
-                <li>
-                  <div>
-                    <Image
-                      className="w-[clamp(4.5rem,7vw,5.625rem)]"
-                      src={MarquesFacebook}
-                      alt="Foto de Pablo Marques e Gabriel Leão"
-                    />
-                  </div>
-                  <div>
-                    <p className="text-[clamp(.875rem,1.5vw,1.125rem)] mb-3">@marqueseleao</p>
-                    <Link
-                      className="flex items-center font-semibold text-sm gap-2 bg-mainPurple rounded-[.635rem] py-3 px-5"
-                      href=""
-                    >
-                      <Image
-                        src={FacebookIcon}
-                        alt="Facebook"
-                      />
-                      Seguir no Facebook
-                    </Link>
-                  </div>
+                  <p className="max-w-[15ch] leading-[1rem] lg:leading-[1.5rem] md:max-w-auto">Seja parceiro Marques&Leão</p>
                 </li>
               </ul>
             </div>
-            <div className="flex gap-[clamp(1rem,1.5vw,2rem)] after:bg-mainPurple after:w-[2px] after:inline-block after:h-48 lg:after:content-none lg:before:bg-mainPurple lg:before:w-[2px] lg:before:inline-block lg:before:h-48">
-              <h3 className="sm:text-xl lg:text-[clamp(.875rem,4vw,1.5rem)] text-end lg:text-start text-[#707070] mt-6 leading-7 lg:max-w-[9ch]">As mentes por trás do negócio</h3>
+          </div>
+        </section>
+        <section className="relative">
+          <div className="hidden lg:block absolute overflow-hidden right-0 bottom-[-30%] -z-10 w-[35%] aspect-square">
+            <Image
+              draggable={false}
+              className="absolute right-[-40%] bottom-0 opacity-75 -z-10 w-full h-full"
+              src={Ellipse}
+              alt="Ellipse blur"
+            />
+          </div>
+          <div className="w-[min(90%,80rem)] mt-36 mx-auto flex flex-col lg:flex-row items-center gap-[clamp(1rem,2.5vw,2.5rem)]">
+            <div className="w-[min(100%,37.5rem)] relative lg:flex-1">
+              <MarquesAndLeaoImg />
+            </div>
+            <div className="w-[min(100%,37.5rem)] sm:mt-4 lg:mt-0 lg:flex-1 flex flex-row-reverse lg:flex-row gap-[clamp(1rem,2vw,2.5rem)]">
+              <div className="w-full lg:max-w-[32ch]">
+                <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-[clamp(2rem,3vw,3rem)] font-bold lg:!leading-[clamp(2rem,4vw,3.5rem)] font-baskervville">Pablo Marques & Gabriel Leão</h2>
+                <p className="sm:text-lg lg:text-lg text-[#d2d2d2] !leading-6 mt-2">Nosso trabalho tem como foco a <span className="text-white font-semibold">conexão entre tecnologia e atendimento de alta performance</span>, com o objetivo de otimizar a jornada e melhorar experiência do cliente.</p>
+                <ul className="hidden lg:block *:flex *:items-center *:gap-5 *:mt-8">
+                  <li>
+                    <div>
+                      <Image
+                        className="w-[clamp(4.5rem,7vw,5.625rem)]"
+                        src={MarquesInstagram}
+                        alt="Foto de Pablo Marques e Gabriel Leão"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-[clamp(.875rem,1.5vw,1.125rem)] mb-3">@marqueseleao</p>
+                      <Link
+                        className="flex items-center font-semibold text-sm gap-2 bg-mainPurple rounded-[.635rem] py-3 px-5"
+                        href="#"
+                      >
+                        <Image
+                          src={InstagramIcon}
+                          alt="Instagram"
+                        />
+                        Seguir no Instagram
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <Image
+                        className="w-[clamp(4.5rem,7vw,5.625rem)]"
+                        src={MarquesYoutube}
+                        alt="Foto de Pablo Marques e Gabriel Leão"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-[clamp(.875rem,1.5vw,1.125rem)] mb-3">@ImobiliariaMarquesLeao</p>
+                      <Link
+                        className="flex items-center font-semibold text-sm gap-2 max-w-[23ch] bg-mainPurple rounded-[.635rem] py-3 px-5"
+                        href=""
+                      >
+                        <Image
+                          className="w-[1.0625rem]"
+                          src={YoutubeIcon}
+                          alt="Youtube"
+                        />
+                        Se inscreva no canal
+                      </Link>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <Image
+                        className="w-[clamp(4.5rem,7vw,5.625rem)]"
+                        src={MarquesFacebook}
+                        alt="Foto de Pablo Marques e Gabriel Leão"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-[clamp(.875rem,1.5vw,1.125rem)] mb-3">@marqueseleao</p>
+                      <Link
+                        className="flex items-center font-semibold text-sm gap-2 bg-mainPurple rounded-[.635rem] py-3 px-5"
+                        href=""
+                      >
+                        <Image
+                          src={FacebookIcon}
+                          alt="Facebook"
+                        />
+                        Seguir no Facebook
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex gap-[clamp(1rem,1.5vw,2rem)] after:bg-mainPurple after:w-[2px] after:inline-block after:h-48 lg:after:content-none lg:before:bg-mainPurple lg:before:w-[2px] lg:before:inline-block lg:before:h-48">
+                <h3 className="sm:text-xl lg:text-[clamp(.875rem,4vw,1.5rem)] text-end lg:text-start text-[#707070] mt-6 leading-7 lg:max-w-[9ch]">As mentes por trás do negócio</h3>
+              </div>
             </div>
           </div>
         </section>
