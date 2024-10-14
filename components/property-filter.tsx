800b0c5e60046a536f1a318f6526e5bf84e14b5f
Raphael Martinez <raphael.martinez@usystem.com.br>
2024-10-14 15:21:40 -0300

    fix: repair filter

--- components/property-filter.tsx
+++ components/property-filter.tsx
@@ -7,7 +7,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useEffect, useRef, useState, FormEvent } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 import { Cidade, Estado } from "smart-imob-types";
@@ -20,7 +20,7 @@ const sideVariants = {
   },
   open: {
     display: "var(--display-to, block)",
-    opacity: "var(--opacity-to, 1)",
+    opacity: "var(--opacity-to, 9)",
   },
 };
 
@@ -135,141 +135,147 @@ const SearchPropertyFilter = ({
 
   return (
     <form
-      className={cn(
-        "group w-[min(100%,62.5rem)] bg-white py-3 px-3 md:py-4 md:px-5 rounded-[.625rem]",
-        className
-      )}
       ref={inputRef}
       onSubmit={(e: FormEvent) => e.preventDefault()}
+      className={cn(
+        "group w-[min(100%,71.875rem)] bg-white py-3 px-3 rounded-[.625rem]",
+        className
+      )}
     >
       <div className="flex justify-between">
         <input
           type="text"
           placeholder="Clique para iniciar sua busca"
-          className="w-full md:flex-1 pl-8 sm:pl-10 md:pl-12 bg-hero-input bg-[size:1.5rem] sm:bg-[size:auto] bg-no-repeat bg-left placeholder:text-black placeholder:text-sm md:placeholder:text-base placeholder:italic text-black outline-none"
+          className="w-full sm:flex-1 pl-6 sm:pl-10 bg-hero-input bg-no-repeat bg-[size:1.25rem] sm:bg-[size:auto] bg-left placeholder:text-black placeholder:text-[.75rem] md:placeholder:text-base placeholder:italic text-black outline-none"
           onClick={toggleMenu}
           readOnly
           value={getSelectedFilters()}
         />
         <Link
+          className="bg-[#2a2b2f] text-[.75rem] flex-shrink-0 py-2 px-4 lg:px-6 rounded-lg"
           href={{
             pathname: "/imoveis",
             query: query,
           }}
-          className="bg-[#2a2b2f] flex-shrink-0 text-[.75rem] md:text-sm py-2 px-4 md:px-6 rounded-lg text-white"
         >
           Buscar imóveis
         </Link>
       </div>
       <motion.div
-        className="bg-white [--display-from:none] [--display-to:block] md:[--display-to:flex] [--opacity-from:0] [--opacity-to:80%] *:text-black *:font-semibold absolute py-4 px-5 w-full bottom-0 translate-y-full left-0 md:gap-3 rounded-[.625rem] *:flex *:flex-wrap md:*:flex-nowrap *:justify-center md:*:justify-between *:items-center"
+        className="bg-white [--display-from:none] [--display-to:block] [--opacity-from:0] [--opacity-to:90%] *:text-black *:font-semibold z-50 absolute py-4 px-5 w-full bottom-0 translate-y-full left-0 md:gap-3 rounded-[.625rem] "
         initial={false}
         animate={isOpen ? "open" : "closed"}
         variants={sideVariants}
       >
-        <div className="md:w-[55%] *:w-[10.5rem] gap-2 *:rounded-xl *:border-black *:border">
-          <Select value={estado} onValueChange={setEstado}>
-            <SelectTrigger>
-              <SelectValue placeholder="Estados" />
-            </SelectTrigger>
-            <SelectContent>
-              {estados.map((estadoItem) => (
-                <SelectItem
-                  key={estadoItem.value}
-                  value={estadoItem.value.toString()}
-                >
-                  {estadoItem.nome}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-
-          <Select value={cidade} onValueChange={setCidade} disabled={!estado}>
-            <SelectTrigger>
-              <SelectValue placeholder="Cidades" />
-            </SelectTrigger>
-            <SelectContent>
-              {cidades
-                // .filter((cidadeItem) => cidadeItem.estado_id.toString() === estado)
-                .map((cidadeItem) => (
+        <div className="max-w-[37.5rem] px-3 mx-auto *:flex *:flex-wrap *:justify-center *:items-center">
+          <div className="*:w-[10.5rem] gap-4 *:rounded-xl *:border-black *:border">
+            <Select value={estado} onValueChange={setEstado}>
+              <SelectTrigger>
+                <SelectValue placeholder="Estados" />
+              </SelectTrigger>
+              <SelectContent>
+                {estados.map((estadoItem) => (
                   <SelectItem
-                    key={cidadeItem.value}
-                    value={cidadeItem.value.toString()}
+                    key={estadoItem.value}
+                    value={estadoItem.value.toString()}
                   >
-                    {cidadeItem.nome}
+                    {estadoItem.nome}
                   </SelectItem>
                 ))}
-            </SelectContent>
-          </Select>
+              </SelectContent>
+            </Select>
 
-          <Select value={bairro} onValueChange={setBairro} disabled={!cidade}>
-            <SelectTrigger>
-              <SelectValue placeholder="Bairros" />
-            </SelectTrigger>
-            <SelectContent>
-              {bairros
-                // .filter((bairroItem: any) => bairroItem.cidadeId.toString() === cidade)
-                .map((bairroItem: any, index: number) => (
-                  <SelectItem key={index} value={bairroItem}>
-                    {bairroItem}
+            <Select value={cidade} onValueChange={setCidade} disabled={!estado}>
+              <SelectTrigger>
+                <SelectValue placeholder="Cidades" />
+              </SelectTrigger>
+              <SelectContent>
+                {cidades
+                  // .filter((cidadeItem) => cidadeItem.estado_id.toString() === estado)
+                  .map((cidadeItem) => (
+                    <SelectItem
+                      key={cidadeItem.value}
+                      value={cidadeItem.value.toString()}
+                    >
+                      {cidadeItem.nome}
+                    </SelectItem>
+                  ))}
+              </SelectContent>
+            </Select>
+
+            <Select value={bairro} onValueChange={setBairro} disabled={!cidade}>
+              <SelectTrigger>
+                <SelectValue placeholder="Bairros" />
+              </SelectTrigger>
+              <SelectContent>
+                {bairros
+                  // .filter((bairroItem: any) => bairroItem.cidadeId.toString() === cidade)
+                  .map((bairroItem: any, index: number) => (
+                    <SelectItem key={index} value={bairroItem}>
+                      {bairroItem}
+                    </SelectItem>
+                  ))}
+              </SelectContent>
+            </Select>
+
+            <Select value={tipo} onValueChange={setTipo}>
+              <SelectTrigger>
+                <SelectValue placeholder="Tipos" />
+              </SelectTrigger>
+              <SelectContent>
+                {tipos.map((tipoItem, index) => (
+                  <SelectItem key={index} value={tipoItem}>
+                    {tipoItem}
                   </SelectItem>
                 ))}
-            </SelectContent>
-          </Select>
+              </SelectContent>
+            </Select>
 
-          <Select value={tipo} onValueChange={setTipo}>
-            <SelectTrigger>
-              <SelectValue placeholder="Tipos" />
-            </SelectTrigger>
-            <SelectContent>
-              {tipos.map((tipoItem, index) => (
-                <SelectItem key={index} value={tipoItem}>
-                  {tipoItem}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-
-          <Select value={codigo} onValueChange={setCodigo}>
-            <SelectTrigger>
-              <SelectValue placeholder="Códigos" />
-            </SelectTrigger>
-            <SelectContent>
-              {codigos.map((codigoItem, index) => (
-                <SelectItem key={index} value={codigoItem}>
-                  {codigoItem}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="relative justify-between w-full flex-col mt-3 md:mt-0 *:w-full *:flex *:justify-between *:border-black *:border *:rounded-lg md:flex-row *:text-sm gap-3 *:py-2 *:px-3 lg:before:bg-black lg:before:h-full lg:before:absolute lg:before:w-[1px]">
-          <label className="md:ml-4">
-            Valor mínimo
-            <input
-              placeholder="R$0,00"
-              className="outline-none w-16 placeholder:text-black"
-              type="number"
-              value={valorMin}
-              onChange={(e) =>
-                setValorMin(e.target.value === "" ? "" : Number(e.target.value))
-              }
-              min={0}
-            />
-          </label>
-          <label>
-            Valor máximo
-            <input
-              placeholder="R$0,00"
-              className="outline-none w-16 placeholder:text-black"
-              type="number"
-              value={valorMax}
-              onChange={(e) =>
-                setValorMax(e.target.value === "" ? "" : Number(e.target.value))
-              }
-              min={0}
-            />
-          </label>
+            <Select value={codigo} onValueChange={setCodigo}>
+              <SelectTrigger>
+                <SelectValue placeholder="Códigos" />
+              </SelectTrigger>
+              <SelectContent>
+                {codigos.map((codigoItem, index) => (
+                  <SelectItem key={index} value={codigoItem}>
+                    {codigoItem}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="relative justify-between w-full md:w-[80%] md:mx-auto flex-col mt-3 *:w-full *:flex *:justify-between *:border-black *:border *:rounded-lg gap-3 *:py-2 *:px-3">
+            <label>
+              Valor mínimo
+              <input
+                placeholder="R$0,00"
+                className="outline-none w-16 placeholder:text-black"
+                type="number"
+                value={valorMin}
+                onChange={(e) =>
+                  setValorMin(
+                    e.target.value === "" ? "" : Number(e.target.value)
+                  )
+                }
+                min={0}
+              />
+            </label>
+            <label>
+              Valor máximo
+              <input
+                placeholder="R$0,00"
+                className="outline-none w-16 placeholder:text-black"
+                type="number"
+                value={valorMax}
+                onChange={(e) =>
+                  setValorMax(
+                    e.target.value === "" ? "" : Number(e.target.value)
+                  )
+                }
+                min={0}
+              />
+            </label>
+          </div>
         </div>
       </motion.div>
     </form>
