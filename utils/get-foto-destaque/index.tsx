609d7e31f5e7e4623725f9198e298cc865ecda46
Raphael Martinez <raphael.martinez@usystem.com.br>
2024-11-01 08:25:47 -0300

    fix: repair photos

--- utils/get-foto-destaque/index.tsx
+++ utils/get-foto-destaque/index.tsx
@@ -1,20 +1,24 @@
 import { Imóvel } from "smart-imob-types";
 
-export const getFotoDestaque = (
-  imovel: Imóvel,
-  resized?: boolean
-): undefined | string => {
-  const fotos = imovel?.fotos || [];
+export const getFotoDestaque = (imovel: Imóvel, resized?: boolean): undefined | string => {
+  const fotos = imovel.fotos || [];
+  const firstImage = fotos.find(image => image.destaque);
+  if (firstImage) {
+    if (resized) {
+      return firstImage.resized || firstImage.source.uri;
+    } else {
+      return firstImage.source.uri;
+    }
+  }
+
   const foto =
-    (imovel?.foto_destaque_index !== undefined
-      ? fotos[imovel?.foto_destaque_index]
-      : null) ||
-    fotos.find((foto) => foto.destaque) ||
+    (imovel.foto_destaque_index !== undefined ? fotos[imovel.foto_destaque_index] : null) ||
+    fotos.find(foto => foto.destaque) ||
     fotos[0];
   if (!foto) return undefined;
   if (resized) {
-    return foto.resized || foto?.source?.uri;
+    return foto.resized || foto.source.uri;
   } else {
-    return foto?.source?.uri;
+    return foto.source.uri;
   }
 };
