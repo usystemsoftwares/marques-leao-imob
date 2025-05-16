d0445be17b53859fbc9a3013f2eca9b15f644f05
Raphael Martinez <raphael.martinez@usystem.com.br>
2025-05-16 12:30:58 -0300

    fix: change btn whatsapp

--- components/whatsapp-btn.tsx
+++ components/whatsapp-btn.tsx
@@ -8,12 +8,7 @@ import { Empresa } from "smart-imob-types";
 export const WhatsappButton = ({ empresa }: { empresa: Empresa }) => {
   return (
     <div className="fixed w-[min(3.125rem,100%)] md:w-[min(4.375rem,100%)] aspect-square right-[3.125rem] bottom-[2rem] md:bottom-[1.625rem] z-[51]">
-      <Link
-        href={
-          "https://s.tintim.app/whatsapp/a9760b6e-fc26-4493-b2e3-7ff652429152/95b5bf55-8193-4994-adb1-92d308cc6b49"
-        }
-        target="_blank"
-      >
+      <Link href={"https://wa.me/555193209037"} target="_blank">
         <Image
           className="w-full"
           src={Whatsapp}
