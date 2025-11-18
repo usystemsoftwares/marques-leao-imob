5bc61b2b79cce7382ead34664abdd408679910dd
Raphael Martinez <raphael.martinez@usystem.com.br>
2025-11-18 15:41:15 -0300

    fix: client api

--- middleware.ts
+++ middleware.ts
@@ -30,11 +30,11 @@ export function middleware(request: NextRequest) {
   // Content Security Policy para prevenir XSS
   const csp = [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com https://*.googletagmanager.com",
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://ssl.google-analytics.com https://*.googletagmanager.com https://maps.googleapis.com https://connect.facebook.net https://www.clarity.ms",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-    "img-src 'self' data: https: blob:",
+    "img-src 'self' data: https: blob: https://firebasestorage.googleapis.com https://storage.googleapis.com https://*.google.com.br https://googleads.g.doubleclick.net https://www.googleadservices.com",
     "font-src 'self' https://fonts.gstatic.com",
-    "connect-src 'self' https://api.smtximob.com https://api.smtximob.com.br https://www.google-analytics.com https://analytics.google.com https://www.google.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com",
+    "connect-src 'self' https://smtximob.com https://api.smtximob.com https://api.smtximob.com.br https://www.google-analytics.com https://analytics.google.com https://www.google.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://stats.g.doubleclick.net https://*.google.com.br https://servidor.marqueseleao.com",
     "frame-src 'self' https://www.googletagmanager.com https://*.googletagmanager.com",
     "media-src 'self' https://firebasestorage.googleapis.com https://storage.googleapis.com data: blob:",
   ].join('; ');
